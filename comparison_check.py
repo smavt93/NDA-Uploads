383715f51e699305dc84d17663718b345b62a195
Sara VT <111016536+smavt93@users.noreply.github.com>
2023-01-18 15:59:46 -0500

    added demo01 file creation

--- comparison_check.py
+++ comparison_check.py
@@ -3,12 +3,14 @@ import numpy as np
 import streamlit as st
 from itertools import islice
 from datetime import date, datetime
+import time
 pd.options.mode.chained_assignment = None
 
 ndar_crosswalk = "https://github.com/smavt93/NDA-Uploads/blob/main/ndar_subject01_crosswalk.xlsx?raw=true"
 ndar_template = "https://raw.githubusercontent.com/smavt93/NDA-Uploads/main/ndar_subject01_template.csv"
 wasi_crosswalk = ""
-demo01_crosswalk = ""
+demo01_crosswalk = "https://github.com/smavt93/NDA-Uploads/blob/main/demo01_crosswalk.xlsx?raw=true"
+demo01_template = "https://github.com/smavt93/NDA-Uploads/raw/main/demo01_template.csv"
 matrics01_crosswalk = ""
 topf01_crosswalk = ""
 today = date.today()
@@ -22,6 +24,7 @@ if operation_selection == "--":
     st.markdown('- "--" is the default setting which just shows you this home page.')
     st.markdown('- "Create Docs" will give you the option to create any of the five NDA docs needed for the upload. Exlcuding the SCID data for now (1-17-23).')
     st.markdown('- "QC" will check previous uploads against your new upload file and will flag any discrepancies.')
+    st.markdown('- "QC" will also check the overall RC database for any imputations or missing data! (Would recommend doing this step first before creating docs).')
     st.markdown('### Regardless of what you choose, you will always need to have the previous upload docs on hand for this dashboard to work.')
 
 @st.cache
@@ -40,7 +43,7 @@ if operation_selection == "Create Docs":
     with tab1:
         st.header("Ndar_subjects01")
         st.markdown("### Before you get started")
-        st.markdown("1. Files you will need are the full db from RC, the previous upload document as well as the phenotype export from Wil")
+        st.markdown("1. Files you will need are (1) the full db from RC, (2) the previous upload document, and (3) the phenotype export from Wil.")
         st.markdown("2. After files are processed you should proceed to the QC portion of the dashboard to finalize the document.")
         col1, col2 = st.columns(2)
         with col1:
@@ -77,9 +80,9 @@ if operation_selection == "Create Docs":
                 rc_columns_to_pull = crosswalk_db['rc_columns_to_pull'].values.tolist()
                 crosswalk_dict = {cleaned_rc_columns_to_change[i]: cleaned_nda_columns_to_change[i] for i in range(len(cleaned_rc_columns_to_change))}
                 # Desired columns
-                desired_columns = second_filter_db.loc[:, rc_columns_to_pull]
+                ndar_desired_columns = second_filter_db.loc[:, rc_columns_to_pull]
                 # Renaming columns
-                ndar_subjects = desired_columns.rename(columns = crosswalk_dict)
+                ndar_subjects = ndar_desired_columns.rename(columns = crosswalk_dict)
 
                 # Filling na items with -8
                 ndar_subjects.fillna(-8,inplace=True)
@@ -193,11 +196,213 @@ if operation_selection == "Create Docs":
 
     with tab2:
         st.header("Demo01")
+        st.markdown("### Before you get started:")
+        st.markdown("1. Files needed are (1) the full RC data export, (2) the previous upload document, and (3) the educ_yrs file from Wil.")
+        st.markdown("2. After files are processed, you should proceed to the QC portion of the dashboard to finalize the document")
         col1, col2 = st.columns(2)
         with col1:
             full_database = st.file_uploader("Full RC data export:", key = "demo")
         with col2:
             previous_demo = st.file_uploader("Previous Demo01 Upload Doc:")
+        if full_database is not None:
+            if previous_demo is not None:
+                educ_yrs_file = st.file_uploader("Educ_yrs document from Wil [EXCEL]")
+                full_db = pd.read_csv(full_database)
+                
+                # Creating a list of excluded subjects that is adaptable
+                reliability_db = full_db[full_db['subject_id'].str.contains('R')] # Removing the reliability subjects
+                reliability_list = reliability_db['subject_id'].tolist()
+                
+                # Exclusion princples: visit_1_arm_1, doesn't have a GUID or Pseudoguid AND is excluded (47)
+                excluded_subject_db_1 = full_db.loc[(full_db['redcap_event_name'] == 'visit_1_arm_1')]
+                excluded_subject_db_2 = excluded_subject_db_1.loc[((excluded_subject_db_1['guid'].isna()) & (excluded_subject_db_1['pseudoguid'].isna()))]
+                excluded_subject_db_3 = excluded_subject_db_2.query("dx1 in [47] or subject_id in ['10055-20', '10178-20']")
+                excluded_subject_filter_list = excluded_subject_db_3['subject_id'].values.tolist()
+                final_excluded_subject_list = excluded_subject_filter_list + reliability_list # Final list of excluded subjects
+
+                # Filtering for the desired datatype
+                first_filter_db = full_db.loc[(full_db['scid_iscomplete'].notnull())]
+                second_filter_db = first_filter_db.query("subject_id not in @final_excluded_subject_list")
+                
+                # Renaming columns and pulling the desired columns
+                # Getting things ready
+                crosswalk_db = pd.read_excel(demo01_crosswalk)
+                rc_columns_to_change = crosswalk_db.rc_rename_columns.values.tolist()
+                cleaned_rc_columns_to_change = [x for x in rc_columns_to_change if str(x) != 'nan']
+                nda_columns_to_change = crosswalk_db['nda_column_names'].values.tolist()
+                cleaned_nda_columns_to_change = [x for x in nda_columns_to_change if str(x) != 'nan']
+                rc_columns_to_pull = crosswalk_db['rc_columns_to_pull'].values.tolist()
+                crosswalk_dict = {cleaned_rc_columns_to_change[i]: cleaned_nda_columns_to_change[i] for i in range(len(cleaned_rc_columns_to_change))}
+                # Desired columns
+                demo01_desired_columns = second_filter_db.loc[:, rc_columns_to_pull]
+                # Renaming columns
+                demo01_subjects = demo01_desired_columns.rename(columns = crosswalk_dict)
+
+                # Filling na items with -8
+                demo01_subjects.fillna(-8,inplace=True)
+
+                # Using the pseudoguid values if subject has no guid but does have pseudoguid data # for this dataset it doesn't apply but a good check regardless
+                demo01_subjects['subjectkey'] = demo01_subjects.apply(lambda row: row['pseudoguid'] if row['subjectkey'] == -8 else row['subjectkey'], axis = 1)
+
+                #Converting the race items to the appropriate NDA format
+                def cat1(row):
+                    if row['race'] == 0:
+                        return 'Declined to Answer'
+                    if row['race'] == 1:
+                        return 'American Indian/Alaskan Native'
+                    if row['race'] == 2:
+                        return 'Asian'
+                    if row['race'] == 3:
+                        return 'Black or African American'
+                    if row['race'] == 4:
+                        return 'Hawaiian or Pacific Islander'
+                    if row['race'] == 5:
+                        return 'White'
+                    if row['race'] == 6:
+                        return 'Unknown or not reported'
+
+                demo01_subjects['race'] = demo01_subjects.apply(lambda row: cat1(row), axis=1)
+
+                #Converting the relationship status to the appropriate NDA format
+                def cat2(row):
+                    if row['das1ms'] == 0:
+                        return 'Never Married'
+                    if row['das1ms'] == 1:
+                        return 'Married'
+                    if row['das1ms'] == 2:
+                        return 'Separated'
+                    if row['das1ms'] == 3:
+                        return 'Divorced'
+                    if row['das1ms'] == 4:
+                        return 'Divorced and Remarried'
+                    if row['das1ms'] == 5:
+                        return 'Widowed'
+                    if row['das1ms'] == 6:
+                        return 'Widowed and Remarried'
+                    if row['das1ms'] == 7:
+                        return 'Living with SO'
+                    if row['das1ms'] == 8:
+                        return 'Other'
+
+                demo01_subjects['das1ms'] = demo01_subjects.apply(lambda row: cat2(row), axis = 1)
+
+                # Converting the integer representation of sex items to string characters
+                def cat3(row):
+                    if row['sex'] == 0:
+                        return 'M'
+                    if row['sex'] == 1:
+                        return 'F'
+
+                demo01_subjects['sex'] = demo01_subjects.apply(lambda row: cat3(row), axis = 1)
+
+                # Handling the married column
+                def cat4(row):
+                    if row['das1ms'] == 'Married':
+                        return 1
+                    else:
+                        return 0
+
+                demo01_subjects['marital2'] = demo01_subjects.apply(lambda row: cat4(row), axis = 1)
+
+                # Handling the significant other column
+                def cat5(row):
+                    if row['das1ms'] == 'Married' or row['das1ms'] == 'Living with SO':
+                        return 1
+                    else:
+                        return 0
+
+                demo01_subjects['sigother'] = demo01_subjects.apply(lambda row: cat5(row), axis = 1)
+
+                # Handling the white race column
+                def cat6(row):
+                    if row['race'] == 'White':
+                        return 1
+                    else:
+                        return 0
+
+                demo01_subjects['white'] = demo01_subjects.apply(lambda row: cat6(row), axis = 1)
+
+                # Handling the black race column
+                def cat7(row):
+                    if row['race'] == 'Black or African American':
+                        return 1
+                    else:
+                        return 0
+
+                demo01_subjects['black'] = demo01_subjects.apply(lambda row: cat7(row), axis = 1)
+
+                # Handling the native race column
+                def cat8(row):
+                    if row['race'] == 'American Indian/Alaskan Native':
+                        return 1
+                    else:
+                        return 0
+
+                demo01_subjects['native'] = demo01_subjects.apply(lambda row: cat8(row), axis = 1)
+
+                # Handling the asian race column
+                def cat9(row):
+                    if row['race'] == 'Asian':
+                        return 'Yes'
+                    else:
+                        return 'No'
+
+                demo01_subjects['asian'] = demo01_subjects.apply(lambda row: cat9(row), axis=1)
+
+                # Handling the pacific race column
+                def cat10(row):
+                    if row['race'] == 'Hawaiian or Pacific Islander':
+                        return 'Yes'
+                    else:
+                        return 'No'
+
+                demo01_subjects['pacific'] = demo01_subjects.apply(lambda row:cat10(row), axis = 1)
+
+                # Converting the subject's age at interview into months per NDA format
+                demo01_subjects['interview_date'] = pd.to_datetime(demo01_subjects['interview_date'])
+                demo01_subjects['dob'] = pd.to_datetime(demo01_subjects['dob'])
+                demo01_subjects['interview_age'] = ((demo01_subjects.interview_date - demo01_subjects.dob)/np.timedelta64(1, 'M'))
+                demo01_subjects['interview_age'] = demo01_subjects['interview_age'].astype(int) # Still problems with dates but don't have time to figure them out now
+                demo01_subjects['interview_date'] = demo01_subjects['interview_date'].dt.strftime("%m/%d/%y") # prevents the export from having the time component as well (not needed)
+                
+                # Removing the pseudoguid and dob columns as those were just needed to get the desired column values. educ_yrs dropped to be replaced with correct values
+                demo01_subjects.drop(['pseudoguid', 'dob', 'educ_yrs'], axis = 1, inplace= True)
+
+                ########## Replacing incorrect edu_yrs with correct ones #######################
+                if educ_yrs_file is not None:
+                    educ_yrs_db = pd.read_excel(educ_yrs_file)
+                    # Getting the correct subject IDs from the demo01 file to filter the educ_yr file
+                    col_list = demo01_subjects['src_subject_id'].values.tolist()
+                    complete_edu = educ_yrs_db.loc[educ_yrs_db['subject_id'].isin(col_list)]
+                    educ_yrs_column = complete_edu[['subject_id','educ_yrs']]
+                    
+                    # Creating a file that has the correct educ_yrs info
+                    correct_edu_col_db = pd.merge(demo01_subjects, educ_yrs_column, how = 'outer', left_on = ['src_subject_id'], right_on= ['subject_id'])
+                    correct_edu_col_db.drop(['subject_id'], axis = 1, inplace=True)
+
+                    # Setting the index column in order to facilitate the concat with template file
+                    correct_edu_col_db_prep = correct_edu_col_db.set_index('subjectkey')
+                    
+                    # Join file created to get data in desired format
+                    demo01_template_db = pd.read_csv(demo01_template, index_col = 'subjectkey')
+                    final_demo01_join_file = pd.concat([demo01_template_db,correct_edu_col_db_prep])
+                    
+                    # Getting the legacy subject_ids
+                    previous_upload_demo = pd.read_csv(previous_demo, skiprows = 1)
+                    previous_subject_id_list_demo = previous_upload_demo['src_subject_id'].values.tolist()
+                    current_subject_id_list_demo = final_demo01_join_file['src_subject_id'].values.tolist()
+                    legacy_list_demo = [x for x in previous_subject_id_list_demo if x not in current_subject_id_list_demo]
+                    legacy_db = previous_upload_demo.query("src_subject_id in @legacy_list_demo")
+                    legacy_db.set_index('subjectkey', inplace = True)
+                    
+                    # Creating final document
+                    final_demo01_file = pd.concat([final_demo01_join_file,legacy_db])
+                    final_demo01_file.sort_values(by=['src_subject_id'], inplace=True)
+                    final_demo01_file = final_demo01_file.loc[:, ~final_demo01_file.columns.str.match('Unnamed')]
+                    st.write(final_demo01_file)
+                    st.write("Number of subjects:", len(final_demo01_file['src_subject_id']))
+                    csv = convert_df(final_demo01_file)
+                    st.download_button("Download Data as a CSV", data = csv, file_name=f'demo01_subject_export {today}.csv', mime = 'text/csv')
     
     with tab3:
         st.header("Wasi201")
